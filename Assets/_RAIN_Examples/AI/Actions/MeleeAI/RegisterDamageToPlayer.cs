07ff36b857eb09a49d8c5b0a21a2c3a58c42a810
Sacristan <girts.kesteris@gmail.com>
2015-04-19 15:04:16 +0300

    Renamed RaptorAI -> RAINExamples

---
+++ Assets/_RAIN_Examples/AI/Actions/MeleeAI/RegisterDamageToPlayer.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+using RAIN.Representation;
+
+
+namespace Sacristan.MeleeAI
+{
+    [RAINAction]
+    public class RegisterDamageToPlayer : RAINAction
+    {
+        public Expression Damage = new Expression();
+        public Expression DamagePeriod = new Expression();
+
+        GameObject playerObject;
+
+        float nodeEntryTime;
+        float lastAttackTime;
+
+        private int damage;
+        private float damagePeriod;
+        private string memoryKey = "lastAttackTime";
+
+        public override void Start(RAIN.Core.AI ai)
+        {
+            nodeEntryTime = Time.timeSinceLevelLoad;
+            lastAttackTime = ai.WorkingMemory.GetItem<float>(memoryKey);
+            playerObject = ai.WorkingMemory.GetItem<GameObject>("player");
+
+            damage = System.Int32.Parse(Damage.ToString());
+            damagePeriod = System.Single.Parse(DamagePeriod.ToString());
+        }
+
+        public override ActionResult Execute(RAIN.Core.AI ai)
+        {
+            if (nodeEntryTime - lastAttackTime > damagePeriod)
+            {
+                lastAttackTime = nodeEntryTime;
+                ai.WorkingMemory.SetItem<float>(memoryKey, nodeEntryTime);
+                playerObject.SendMessage("ReceiveDamage", damage);
+                return ActionResult.SUCCESS;
+            }
+            else
+            {
+                return ActionResult.FAILURE;
+            }
+
+
+        }
+
+    }
+}
